c37736d05618b40857d0c3578cb8019aaa507580
Alexander Probst <alexander.probst@sportalliance.com>
2022-02-23 16:46:57 +0100

    create basic cypress project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spa-coding-exercise-qa
+
+**Node** and **yarn** should be installed on your machine
+
+Checkout and run:
+```
+yarn
+```
+
+Try to execute cypress test:
+```
+yarn cypress open
+```
---
+++ cypress/integration/test1.spec.ts
@@ -0,0 +1,9 @@
+//Test should start but is currently broken, we will fix the test together
+it('test1', () => {
+    cy.visit('https://coding-session.web.magicline.com')
+    cy.get("body > div.ml-ref-app.ml-app.ml-app-expanded > div > div > div > div > form > div:nth-child(1)").type("user")
+    cy.get("[data-role=password]").type("123456")
+    cy.contains("Anmelden")
+    cy.wait(5000)
+    cy.get(".ml-main").should("be.visible")
+})
